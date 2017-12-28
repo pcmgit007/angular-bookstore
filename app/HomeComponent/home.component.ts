4040fc4a7ae2bea8ee4bb887c9685fdd4af17867
sneha-me <snehanv24@gmail.com>
2017-12-28 19:58:58 +0530

    css changes
    minor css changes for select dropdown

--- app/HomeComponent/home.component.ts
+++ app/HomeComponent/home.component.ts
@@ -2,9 +2,11 @@
 
 @Component({
     template: `<img src='app/shared/stack-of-books-d-background.jpg' alt="stack of books" />
-                <h3>{{ 'welcomeText' | translate }}</h3>
+                <h3 class="welcome">{{ 'welcomeText' | translate }}</h3>
                 `,
-    styles: ['img{position: absolute; left: 30%; top: 35%}']
+    styles: ['img{position: absolute; left: 30%; top: 35%}',
+        '.welcome{font-size: 39px; top: 45%; position: absolute;left: 50%;}'  
+    ]
 })
 
 export class HomeComponent {}
